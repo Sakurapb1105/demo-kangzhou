ffbaaec856138deef88625351e4d40e899fb916c
pb <1215282843@qq.com>
2026-03-25 01:03:05 +0800

    完成MyBatis-Plus基础代码生成

--- src/main/java/com/example/demokangzhou/DemoKangzhouApplication.java
+++ src/main/java/com/example/demokangzhou/DemoKangzhouApplication.java
@@ -1,8 +1,10 @@
 package com.example.demokangzhou;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.example.demokangzhou.mapper")
 @SpringBootApplication
 public class DemoKangzhouApplication {
 
