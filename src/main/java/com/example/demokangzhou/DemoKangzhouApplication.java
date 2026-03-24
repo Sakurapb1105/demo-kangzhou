3dcd07cf13ef2ba5f53a5ac1bf3236af692a5780
pb <1215282843@qq.com>
2026-03-24 16:38:41 +0800

    chore: 初始化后端项目结构

---
+++ src/main/java/com/example/demokangzhou/DemoKangzhouApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokangzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKangzhouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoKangzhouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demokangzhou/DemoKangzhouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demokangzhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKangzhouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
